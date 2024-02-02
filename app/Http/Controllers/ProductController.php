65a263353545d64b6882141a26c95d6af23d6516
PythonScript1 <hplaptopsimo@gmail.com>
2024-02-02 22:06:55 +0100

    creating the method ajouer product traitment which validates neccessery inputs and then saves the data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,5 +18,22 @@ class ProductController extends Controller
         return view('product.ajouter');
     }
 
-   
+    public function ajouter_product_traitement(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'prix' => 'required',
+            'description' => 'required',
+        ]);
+
+        $product = new Product();
+        $product->nom = $request->nom;
+        $product->prix = $request->prix;
+        $product->description = $request->description; // Correction de la variable description
+        $product->save();
+
+        return redirect('/ajouter')->with('status', 'Le produit a été bien ajouté.'); // Correction du message de succès
+    }
+
+    
 }
